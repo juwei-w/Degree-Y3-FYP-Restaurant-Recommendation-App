452e1cb0ae364558e4fa0da6e63b66a3de9704f8
Ju Wei <114849918+juwei-w@users.noreply.github.com>
2025-06-19 02:11:05 +0800

    move javascript files

--- checkAdmin.js
+++
@@ -1,9 +0,0 @@
-const admin = require("firebase-admin");
-admin.initializeApp({
-  credential: admin.credential.cert(require("./serviceAccountKey.json")),
-});
-
-admin.auth().getUserByEmail("payard@gmail.com")
-  .then(userRecord => {
-    console.log(userRecord.customClaims); // Should show { admin: true }
-  });
